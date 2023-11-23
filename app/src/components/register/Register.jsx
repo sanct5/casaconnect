3a95efcbbcfd31fd2dc91524ff3ea61b2677fab4
isabella202 <isabella.martinez_p@uao.edu.co>
2023-11-22 19:38:47 -0500

    Login y Register
    Se realizo el Front del login y register

--- app/src/components/register/Register.jsx
+++ app/src/components/register/Register.jsx
@@ -1,10 +1,213 @@
-import React from 'react'
-import { Container } from '@mui/material'
+import * as React from "react";
+import { Box, Container } from "@mui/material";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import Logo from "../../assets/Logo.svg";
 
 export const Register = () => {
-    return (
-        <Container disableGutters maxWidth="xl">
-            Register
-        </Container>
-    )
-}
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos del formulario
+    console.log("Formulario enviado:", formData);
+  };
+
+  return (
+    <Container disableGutters maxWidth="xl">
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={5}
+          style={{ backgroundColor: "#3F618C" }}
+        >
+          <img
+            style={{
+              width: "90%",
+              height: "auto",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              margin: "auto",
+              marginTop: "10%",
+            }}
+            src={Logo}
+          />
+        </Grid>
+        <Grid item xs={12} sm={8} md={7} component={Paper} elevation={6} square>
+          <div
+            style={{
+              margin: "center",
+              alignItems: "center",
+              marginTop: "10%",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <div>
+                  <div>
+                    <Typography
+                      variant="h5"
+                      gutterBottom
+                      style={{
+                        color: "#3F618C",
+                        fontFamily: "Poppins",
+                        fontWeight: "bold",
+                        fontSize: 24,
+                      }}
+                    >
+                      Correo*
+                    </Typography>
+                  </div>
+                    <TextField
+                      variant="outlined" // Puedes cambiar a "filled" o "standard" según tus necesidades
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      name="email"
+                      autoComplete="email"
+                      autoFocus
+                      
+                    />
+                </div>
+                <div></div>
+                <div style={{ margin: "10px" }}></div>
+
+                <div>
+                  <div>
+                    <Typography
+                      variant="h5"
+                      gutterBottom
+                      style={{
+                        color: "#3F618C",
+                        fontFamily: "Poppins",
+                        fontWeight: "bold",
+                        fontSize: 24,
+                      }}
+                    >
+                      Contraseña*
+                    </Typography>
+                  </div>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </div>
+                <div style={{ margin: "10px" }}></div>
+                <div>
+                  <div>
+                    <Typography
+                      variant="h5"
+                      gutterBottom
+                      style={{
+                        color: "#3F618C",
+                        fontFamily: "Poppins",
+                        fontWeight: "bold",
+                        fontSize: 24,
+                      }}
+                    >
+                      Confirmar Contraseña*
+                    </Typography>
+                  </div>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </div>
+                <Grid style={{ marginTop: "5%" }}></Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{
+                    mt: 3,
+                    mb: 2,
+                    backgroundColor: "#3F618C",
+                    borderRadius: 8,
+                    fontFamily: "Poppins",
+                    fontWeight: "bold",
+                    fontSize: 24,
+                  }}
+                >
+                  Registrarse
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link
+                      href="#"
+                      variant="body2"
+                      sx={{
+                        color: "#7A7A7A",
+                        fontFamily: "Poppins",
+                        fontWeight: "bold",
+                        fontSize: 18,
+                        marginRight: "15px",
+                      }}
+                    >
+                      ¿Ya tienes una cuenta?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link
+                      href="#"
+                      variant="body2"
+                      sx={{
+                        color: "#406595",
+                        fontFamily: "Poppins",
+                        fontWeight: "bold",
+                        fontSize: 18,
+                      }}
+                    >
+                      {"Inicio de Sesion"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
